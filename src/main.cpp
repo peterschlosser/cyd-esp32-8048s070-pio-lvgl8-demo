e270fffa5e37dc0b41f7f5093ef6c98417819172
Peter Schlosser <commerce@sonic.net>
2025-11-29 12:39:39 -0800

    initial release

---
+++ src/main.cpp
@@ -0,0 +1,208 @@
+#include <Arduino.h>
+#include <LovyanGFX.hpp>
+#include <lgfx/v1/platforms/esp32s3/Panel_RGB.hpp>
+#include <lgfx/v1/platforms/esp32s3/Bus_RGB.hpp>
+#include <driver/i2c.h>
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+
+static const char *TAG = "main"; // logging
+
+class LGFX : public lgfx::LGFX_Device
+{
+public:
+  lgfx::Bus_RGB _bus_instance;
+  lgfx::Panel_RGB _panel_instance;
+  lgfx::Light_PWM _light_instance;
+  lgfx::Touch_GT911 _touch_instance;
+
+  LGFX(void)
+  {
+    {
+      auto cfg = _panel_instance.config();
+
+      cfg.memory_width = 800;
+      cfg.memory_height = 480;
+      cfg.panel_width = 800;
+      cfg.panel_height = 480;
+
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+
+      _panel_instance.config(cfg);
+    }
+
+    {
+      auto cfg = _panel_instance.config_detail();
+
+      cfg.use_psram = 1;
+
+      _panel_instance.config_detail(cfg);
+    }
+
+    {
+      auto cfg = _bus_instance.config();
+      cfg.panel = &_panel_instance;
+      cfg.pin_d0 = GPIO_NUM_15;  // B0
+      cfg.pin_d1 = GPIO_NUM_7;   // B1
+      cfg.pin_d2 = GPIO_NUM_6;   // B2
+      cfg.pin_d3 = GPIO_NUM_5;   // B3
+      cfg.pin_d4 = GPIO_NUM_4;   // B4
+      cfg.pin_d5 = GPIO_NUM_9;   // G0
+      cfg.pin_d6 = GPIO_NUM_46;  // G1
+      cfg.pin_d7 = GPIO_NUM_3;   // G2
+      cfg.pin_d8 = GPIO_NUM_8;   // G3
+      cfg.pin_d9 = GPIO_NUM_16;  // G4
+      cfg.pin_d10 = GPIO_NUM_1;  // G5
+      cfg.pin_d11 = GPIO_NUM_14; // R0
+      cfg.pin_d12 = GPIO_NUM_21; // R1
+      cfg.pin_d13 = GPIO_NUM_47; // R2
+      cfg.pin_d14 = GPIO_NUM_48; // R3
+      cfg.pin_d15 = GPIO_NUM_45; // R4
+
+      cfg.pin_henable = GPIO_NUM_41;
+      cfg.pin_vsync = GPIO_NUM_40;
+      cfg.pin_hsync = GPIO_NUM_39;
+      cfg.pin_pclk = GPIO_NUM_42;
+      cfg.freq_write = 12000000; // reduce jitter
+
+      cfg.hsync_polarity = 0;
+      cfg.hsync_front_porch = 80;
+      cfg.hsync_pulse_width = 4;
+      cfg.hsync_back_porch = 16;
+      cfg.vsync_polarity = 0;
+      cfg.vsync_front_porch = 22;
+      cfg.vsync_pulse_width = 4;
+      cfg.vsync_back_porch = 4;
+      cfg.pclk_idle_high = 1;
+      _bus_instance.config(cfg);
+    }
+    _panel_instance.setBus(&_bus_instance);
+
+    {
+      auto cfg = _light_instance.config();
+      cfg.pin_bl = GPIO_NUM_2;
+      _light_instance.config(cfg);
+    }
+    _panel_instance.light(&_light_instance);
+
+    {
+      auto cfg = _touch_instance.config();
+      cfg.x_min = 0;
+      cfg.x_max = 800;
+      cfg.y_min = 0;
+      cfg.y_max = 480;
+      cfg.pin_int = GPIO_NUM_NC;
+      cfg.bus_shared = true;
+      cfg.offset_rotation = 0;
+      cfg.i2c_port = I2C_NUM_1;
+      cfg.pin_sda = GPIO_NUM_19;
+      cfg.pin_scl = GPIO_NUM_20;
+      cfg.pin_rst = GPIO_NUM_38;
+      cfg.freq = 400000;
+      cfg.i2c_addr = 0x14; // 0x5D , 0x14
+      _touch_instance.config(cfg);
+      _panel_instance.setTouch(&_touch_instance);
+    }
+
+    setPanel(&_panel_instance);
+  }
+};
+
+LGFX gfx;
+
+#define _LV_DISP_DRAW_BUF_SIZE (gfx.width() * gfx.height() * sizeof(lv_color_t) / 8)
+lv_disp_drv_t disp_drv;
+lv_indev_drv_t indev_drv;
+lv_disp_draw_buf_t disp_draw_buf;
+lv_color_t *disp_buf[2];
+
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  if (gfx.getStartCount() == 0)
+  {
+    gfx.startWrite();
+  }
+  gfx.pushImageDMA(area->x1,
+                   area->y1,
+                   area->x2 - area->x1 + 1,
+                   area->y2 - area->y1 + 1,
+                   (lgfx::rgb565_t *)&color_p->full);
+  gfx.endWrite();
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  uint16_t touchX, touchY;
+  const uint32_t touchDebounce = 10; // ms
+  static uint32_t lastTouchTime = 0;
+  if (millis() - lastTouchTime < touchDebounce)
+  {
+    return;
+  }
+
+  bool touched = gfx.getTouch(&touchX, &touchY);
+  data->state = LV_INDEV_STATE_RELEASED;
+
+  if (touched)
+  {
+    lastTouchTime = millis();
+    data->state = LV_INDEV_STATE_PRESSED;
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(100);
+  }
+  ESP_LOGI(TAG, "started.");
+
+  gfx.begin();
+  gfx.initDMA();
+  gfx.setBrightness(128);           // adjust as desired (1-255)
+  gfx.setCursor(0, 0);              // optional
+  gfx.setTextColor(0xFFFF, 0x0000); // optional
+  gfx.setTextSize(1);               // optional
+  gfx.setTextWrap(false);           // optional
+  ESP_LOGI(TAG, "display started.");
+
+  lv_init();
+  disp_buf[0] = (lv_color_t *)heap_caps_aligned_alloc(64, _LV_DISP_DRAW_BUF_SIZE, /* MALLOC_CAP_DMA | */ MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  disp_buf[1] = (lv_color_t *)heap_caps_aligned_alloc(64, _LV_DISP_DRAW_BUF_SIZE, /* MALLOC_CAP_DMA | */ MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (disp_buf[0] == nullptr || disp_buf[1] == nullptr)
+  {
+    ESP_LOGI(TAG, "buffer allocation failure, aborting.");
+    while (1)
+      yield();
+  }
+  lv_disp_draw_buf_init(&disp_draw_buf, disp_buf[0], disp_buf[1], _LV_DISP_DRAW_BUF_SIZE / /* pixel size */ sizeof(lv_color_t));
+
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = gfx.width();
+  disp_drv.ver_res = gfx.height();
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &disp_draw_buf;
+  disp_drv.full_refresh = 0;
+  lv_disp_drv_register(&disp_drv);
+
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  lv_demo_widgets();
+  ESP_LOGI(TAG, "gui started.");
+}
+
+void loop()
+{
+  lv_timer_handler();
+
+  delay(1);
+}
